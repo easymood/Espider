c1bcd1c5fc1b6f4078106ad03fff0b972132b7e9
sam <easymood@gmail.com>
2015-01-27 22:12:08 +0800

    签入配置文件

---
+++ app.js
@@ -0,0 +1,30 @@
+﻿var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+var iconv = require('iconv-lite');
+var BufferHelper = require('bufferhelper');
+var http = require('http');
+
+
+http.get("http://www.amazon.com/senlips-light-bar-13-5inch-combo/dp/B00NYK0FNA/ref=aag_m_pw_dp?ie=UTF8&m=A1ZH4LPYESDYR6", function(res) {
+    var bufferHelper = new BufferHelper();
+
+    res.on('data', function(chunk) {
+
+      bufferHelper.concat(chunk);
+      //console.log(bufferHelper.concat(chunk))
+    });
+    res.on('end', function() {
+      //getWebsiteContent(bufferHelper, objName);
+       var html=bufferHelper.toBuffer().toString();
+
+
+
+       $ = cheerio.load(html);
+       var Pname=$("#btAsinTitle").text();
+       var sku
+       console.log(Pname);
+       //console.log($('#soldByThirdParty span').text());
+
+    });
+  })
