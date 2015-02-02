97f8b9c14d114f2bfd9570dcc166f9bd386f0cd8
sam <easymood@gmail.com>
2015-02-02 22:36:41 +0800

    解决不能循环获取数据的问题

--- start.js
+++ start.js
@@ -8,6 +8,40 @@
   }
 });
 phantom.outputEncoding = "gbk";//解决乱码问题
+
+//调用函数
+var getPlist=function() {  
+   console.log("start getPlist");  
+    productPlist = spider.evaluate(function getPlistFromPage() {  
+      var list=document.querySelectorAll(".shoveler-content ul .aagImgLink"); 
+      var obj=[];
+     for (var i=0;i<list.length;i++)
+     {
+      obj[i]=list[i].href
+      };
+
+     //翻页
+     if (document.querySelector(".next-button-link").className.indexOf("disabled")>0){
+           document.querySelector(".next-button-link").click()
+     }else{
+      console.log("pageer END");  
+      spider.exit(); 
+     }
+  
+      
+     return obj
+    }); 
+
+    console.log(productPlist.length);
+     for (var i=0;i<productPlist.length;i++)
+     {
+       console.log(productPlist[i]);
+      };
+
+    spider.exit(); 
+}
+//
+
 var productPlist;
 //使用CHROME浏览器内核访问
 spider.userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36');
@@ -17,25 +51,7 @@ spider.start();
 spider.thenOpen('http://www.amazon.com/gp/aag/main?ie=UTF8&asin=&isAmazonFulfilled=&isCBA=&marketplaceID=ATVPDKIKX0DER&orderID=&seller=A1ZH4LPYESDYR6'); 
  console.log("start finished");  
 //访问商品列表
-spider.then(function getPlist() {  
-   console.log("start getPlist");  
-    productPlist = spider.evaluate(function getPlistFromPage() {  
-    		return  document.querySelector(".shoveler-content ul"); 
-
-    }); 
-    console.log(productPlist)
-    var Plist=productPlist.getElementsByClassName("aagImgLink")
-  	//循环获取记录列表中的链接
-    for (var i=0;i<Plist.length;i++)
-	{
-	console.log(i);
-	console.log(Plist);
-	console.log(Plist.length);
-	console.log(Plist[i]);
-	}
-
-    spider.exit(); 
-});  
+spider.then(getPlist());  
 //查看商品列表
 spider.run();
 
