6adc5a21657bfcfe202611b77ad806b773253c3b
sam <easymood@gmail.com>
2015-02-21 01:55:49 +0800

    完成带参数调用
    签入带参数调用的代码，可以在NODEJS之下直接调用CASPERJS

--- plist.js
+++ plist.js
@@ -8,13 +8,17 @@ var spider = require('casper').create({
   }
 });
 phantom.outputEncoding = "gbk";//解决乱码问题
+var system = require('system');
+var url = system.args[4];
+
 var productPlist;
 //使用CHROME浏览器内核访问
 spider.userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36');
 //启动浏览器
 spider.start();
 //打开目标网页
-spider.thenOpen('http://www.amazon.com/gp/aag/main?ie=UTF8&asin=&isAmazonFulfilled=&isCBA=&marketplaceID=ATVPDKIKX0DER&orderID=&seller=A8PJP7QGU6HAE'); 
+spider.thenOpen(url); 
+console.log("easy="+url)
 console.log("start finished");  
 //访问商品列表
 
--- start.js
+++ start.js
@@ -3,19 +3,19 @@ console.log('主进程开启');
 var startTime = new Date().getTime();
  
 var https = require('http');
- 
 /*
 启动casperjs读取单个url
 */
 function capture(url) {
-  console.log('打开URL');
+  console.log('打开URL='+url);
   //count++;
 
 
   var spawn = require('child_process').spawn,
   //
  //ls = spawn('casperjs', ['plist.js']);
- ls = spawn('cmd', ['/c','casperjs','plist.js']); //疑惑，为何必须输入一个目录之后才能调用其他API？
+//URL参数供抽取函数调用
+ ls = spawn('cmd', ['/c','casperjs','plist.js',url]); //疑惑，为何必须输入一个目录之后才能调用其他API？
   //var url = system.args[4];
  ls.stdout.on('data', function (data) {
     //do some stuff with each stdout line...
@@ -36,6 +36,7 @@ function capture(url) {
 
 /*
 获取需要采集的url列表
+注意URL必须使用ESCAPE函数转义后，才能使用
 */
 
-capture();
+capture('http://www.amazon.com/gp/aag/main%3Fie%3DUTF8%26asin%3D%26isAmazonFulfilled%3D%26isCBA%3D%26marketplaceID%3DATVPDKIKX0DER%26orderID%3D%26seller%3DA8PJP7QGU6HAE');
